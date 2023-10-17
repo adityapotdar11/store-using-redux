ffc95b1751e33a992a21c977bcefa9f2d676eba3
adityapotdar11 <aditya@webstersolutions.in>
2023-10-17 12:02:30 +0530

    View cart

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,46 +1,69 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { getCart } from "../actions/cart";
 import Ratings from "../Components/Ratings";
+import { Col, Row } from "react-bootstrap";
 import { Trash2 } from "react-feather";
+import { connect } from "react-redux";
 
-const Cart = () => {
+const Cart = ({ getCart, cart }) => {
+    useEffect(() => {
+        getCart();
+    }, [getCart]);
+    console.log(cart);
     return (
         <div className="mt-2">
             <Row className="mb-3">
                 <Col>
-                    <h4>Products</h4>
+                    <h4>Cart</h4>
                 </Col>
             </Row>
             <Row>
-                <Col md={12} className="single-product">
-                    <Row>
-                        <Col md={3} className="product-img-section">
-                            <img
-                                className="product-image"
-                                src="https://img.freepik.com/free-photo/hard-drive-components-laptop_23-2149417054.jpg"
-                                alt="{product.title}"
-                            />
-                        </Col>
-                        <Col md={7} className="product-info-sec">
-                            <h3 className="product-name">Hard Drive</h3>
-                            <p className="product-desc">Fast</p>
-                            <div className="product-star">
-                                <Ratings ratings={5} />
-                            </div>
-                        </Col>
-                        <Col md={2} className="product-action-sec">
-                            <h3 className="product-price">Price: $100</h3>
-                            <div className="product-action">
-                                <span>
-                                    <Trash2 className="feather" />
-                                </span>
-                            </div>
-                        </Col>
-                    </Row>
-                </Col>
+                {cart.map((product) => (
+                    <Col md={12} className="single-product">
+                        <Row>
+                            <Col md={3} className="product-img-section">
+                                <img
+                                    className="product-image"
+                                    src={product.image}
+                                    alt={product.title}
+                                />
+                            </Col>
+                            <Col md={7} className="product-info-sec">
+                                <h3 className="product-name">
+                                    {product.title}
+                                </h3>
+                                <p className="product-desc">{product.desc}</p>
+                                <div className="product-star">
+                                    <Ratings ratings={product.ratings} />
+                                </div>
+                            </Col>
+                            <Col md={2} className="product-action-sec">
+                                <h3 className="product-price">
+                                    Price: ${product.price}
+                                </h3>
+                                <h5>Qty: {product.qty}</h5>
+                                <div className="product-action">
+                                    <span>
+                                        <Trash2 className="feather" />
+                                    </span>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                ))}
             </Row>
         </div>
     );
 };
 
-export default Cart;
+Cart.propTypes = {
+    getCart: PropTypes.func.isRequired,
+    cart: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    cart: state.cart.cart,
+});
+
+export default connect(mapStateToProps, { getCart })(Cart);
